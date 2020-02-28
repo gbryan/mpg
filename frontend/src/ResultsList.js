78a209d0237aa6a9e26b4e93c8013dc4cb35b059
Bryan Gaston <bryangaston122@gmail.com>
2020-02-27 23:11:22 -0800

    Show cylinders and other vehicle fields in results

--- frontend/src/ResultsList.js
+++ frontend/src/ResultsList.js
@@ -31,6 +31,10 @@ class ResultsList extends Component {
                             <th>Year</th>
                             <th>Make</th>
                             <th>Model</th>
+                            <th>Cylinders</th>
+                            <th>Displacement</th>
+                            <th>Drive</th>
+                            <th>Transmission</th>
                         </tr>
                     </thead>
                     <tbody>
@@ -45,6 +49,10 @@ class ResultsList extends Component {
                                 <td><span className={styles.heading}>Year</span>{row.year}</td>
                                 <td><span className={styles.heading}>Make</span>{row.make}</td>
                                 <td><span className={styles.heading}>Model</span>{row.model}</td>
+                                <td><span className={styles.heading}>Cylinders</span>{row.cylinders}</td>
+                                <td><span className={styles.heading}>Displacement</span>{row.displacement}</td>
+                                <td><span className={styles.heading}>Drive</span>{row.drive}</td>
+                                <td><span className={styles.heading}>Transmission</span>{row.transmission}</td>
                             </tr>
                         );
                     })}
@@ -59,7 +67,7 @@ ResultsList.propTypes = {
     values: PropTypes.array.isRequired,
     selectedVehicles: PropTypes.array.isRequired,
     onClick: PropTypes.func.isRequired,
-    vehiclePrices: PropTypes.object.isRequired
+    vehiclePrices: PropTypes.object.isRequired,
 };
 
 export default ResultsList;
